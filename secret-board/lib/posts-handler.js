f6cfcc6204fe726ca5661c399c333226869a78fd
Tsukasa Horinouchi <tsukasa.horinouchi@groove-x.com>
2020-06-03 10:11:54 +0900

    impl cookie

--- secret-board/lib/posts-handler.js
+++ secret-board/lib/posts-handler.js
@@ -2,8 +2,15 @@
 
 const pug = require('pug');
 const Post = require('./post');
+const util = require('./handler-util');
+const Cookies = require('cookies');
+
+const trackingIdKey = 'tracking_id';
 
 function handle(req, res) {
+    const cookies = new Cookies(req, res);
+    addTrackingCookie(cookies);
+
     switch (req.method) {
         case 'GET':
             res.writeHead(200, {
@@ -12,6 +19,11 @@ function handle(req, res) {
             Post.findAll().then((posts) => {
                 res.end(pug.renderFile('./views/posts.pug', {posts: posts}));
             });
+            console.info(
+                `閲覧されました: user: ${req.user}, ` + 
+                `trackingID: ${cookies.get(trackingIdKey)}, ` + 
+                `remoteAddress: ${req.connection.remoteAddress}`
+            );
             break;
         case 'POST':
             let body = [];
@@ -23,7 +35,7 @@ function handle(req, res) {
                 const content = decoded.split('content=')[1];
                 Post.create({
                     content: content,
-                    trackingCookie: null,
+                    trackingCookie: cookies.get(trackingIdKey),
                     postedBy: req.user
                 }).then(() => {
                     handleRedirectPosts(req, res);
@@ -32,6 +44,14 @@ function handle(req, res) {
             break;
         default:
             break;
+    }
+}
+
+function addTrackingCookie(cookies) {
+    if (!cookies.get(trackingIdKey)) {
+        const trakingId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+        const tommorrow = new Date(Date.now() + (1000 * 60 * 60 * 24));
+        cookies.set(trackingIdKey, trakingId, {expires: tommorrow});
     }
 }
 
