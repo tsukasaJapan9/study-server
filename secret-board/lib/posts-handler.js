dc27a8a7d6d148cbb559ca08786983f81985b5dd
Tsukasa Horinouchi <tsukasa.horinouchi@groove-x.com>
2020-06-26 08:45:53 +0900

    add hash

--- secret-board/lib/posts-handler.js
+++ secret-board/lib/posts-handler.js
@@ -4,12 +4,13 @@ const pug = require('pug');
 const Post = require('./post');
 const util = require('./handler-util');
 const Cookies = require('cookies');
+const crypto = require('crypto');
 
 const trackingIdKey = 'tracking_id';
 
 function handle(req, res) {
     const cookies = new Cookies(req, res);
-    addTrackingCookie(cookies);
+    const trackingId = addTrackingCookie(cookies, req.user);
 
     switch (req.method) {
         case 'GET':
@@ -24,7 +25,7 @@ function handle(req, res) {
             });
             console.info(
                 `閲覧されました: user: ${req.user}, ` + 
-                `trackingID: ${cookies.get(trackingIdKey)}, ` + 
+                `trackingID: ${trackingId}, ` + 
                 `remoteAddress: ${req.connection.remoteAddress}`
             );
             break;
@@ -38,7 +39,7 @@ function handle(req, res) {
                 const content = decoded.split('content=')[1];
                 Post.create({
                     content: content,
-                    trackingCookie: cookies.get(trackingIdKey),
+                    trackingCookie: trackingId,
                     postedBy: req.user
                 }).then(() => {
                     handleRedirectPosts(req, res);
@@ -76,13 +77,42 @@ function handleDelete(req, res) {
     }
   }
 
-function addTrackingCookie(cookies) {
-    if (!cookies.get(trackingIdKey)) {
-        const trakingId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-        const tommorrow = new Date(Date.now() + (1000 * 60 * 60 * 24));
-        cookies.set(trackingIdKey, trakingId, {expires: tommorrow});
+/**
+ * Cookieに含まれているトラッキングIDに異常がなければその値を返し、
+ * 存在しない場合や異常なものである場合には、再度作成しCookieに付与してその値を返す
+ * @param {Cookies} cookies
+ * @param {String} userName
+ * @return {String} トラッキングID
+ */
+function addTrackingCookie(cookies, userName) {
+    const requestedTrackingId = cookies.get(trackingIdKey);
+    if (isValidTrackingId(requestedTrackingId, userName)) {
+      return requestedTrackingId;
+    } else {
+      const originalId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+      const tomorrow = new Date(Date.now() + (1000 * 60 * 60 * 24));
+      const trackingId = originalId + '_' + createValidHash(originalId, userName);
+      cookies.set(trackingIdKey, trackingId, { expires: tomorrow });
+      return trackingId;
     }
-}
+  }
+  
+  function isValidTrackingId(trackingId, userName) {
+    if (!trackingId) {
+      return false;
+    }
+    const splitted = trackingId.split('_');
+    const originalId = splitted[0];
+    const requestedHash = splitted[1];
+    return createValidHash(originalId, userName) === requestedHash;
+  }
+  
+  function createValidHash(originalId, userName) {
+    const sha1sum = crypto.createHash('sha1');
+    sha1sum.update(originalId + userName);
+    return sha1sum.digest('hex');
+  }
+  
 
 function handleRedirectPosts(req, res) {
     res.writeHead(303, {
